1e1c531a865085963670afa87d152d0d04f7ef5a
LibXZR <xzr467706992@163.com>
2019-08-06 16:40:23 +0800

    Disable KSM for all devices

--- patches/init.nameless.sh
+++ patches/init.nameless.sh
@@ -9,9 +9,3 @@ else
     echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
     echo madvise > /sys/kernel/mm/transparent_hugepage/defrag
 fi
-
-if [ $MemTotal -lt 3000000 ]; then
-   echo 1 > /sys/kernel/mm/ksm/run
-else
-   echo 0 > /sys/kernel/mm/ksm/run
-fi
