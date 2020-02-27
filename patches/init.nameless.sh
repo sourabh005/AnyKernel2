5304be2cea7920bdebb3856bdbdd248e742d8826
LibXZR <xzr467706992@163.com>
2020-02-27 10:52:32 +0800

    script: Cleanup unused sruff
    * Hugepage isnt used anymore

--- patches/init.nameless.sh
+++ patches/init.nameless.sh
@@ -1,11 +1,2 @@
 #!/system/bin/sh
-MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-MemTotal=${MemTotalStr:16:8}
 
-if [ $MemTotal -lt 5242880 ]; then
-    echo never > /sys/kernel/mm/transparent_hugepage/enabled
-    echo never > /sys/kernel/mm/transparent_hugepage/defrag
-else
-    echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
-    echo madvise > /sys/kernel/mm/transparent_hugepage/defrag
-fi
